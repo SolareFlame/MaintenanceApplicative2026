4d381e93791071eec938386687bbf6fcad6fa3a2
SolareFlame <zeldac332@gmail.com>
2026-03-11 17:38:55 +0100

    Refractor on GildedRose, passed tests

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -3,60 +3,83 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
+    final int MIN_CAP = 0;
+    final int MAX_CAP = 50;
+
+    final String AGED_BRIE = "Aged Brie";
+    final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    final String BACKSTAGE =  "Backstage passes to a TAFKAL80ETC concert";
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    public void updateBaseQuality(Item item) {
+        item.sellIn--; // Baisse du jour
+        item.quality--; // Baisse de la qualité
+
+        if(item.sellIn < 0) item.quality--;
+
+        capQuality(item);
+    }
+
+    public void updateSulfuras(Item leg) {
+        // pas de mouvement sellIn
+        // pas de mouvement quality
+    }
+
+    public void updateBackStage(Item bs) {
+        bs.sellIn--;
+
+        if (bs.sellIn < 0) {
+            bs.quality = 0; // passé
+
+        } else if (bs.sellIn < 5) {
+            bs.quality += 3; // qualité + 3 à 5 jours ou moins
+
+        } else if (bs.sellIn < 10) {
+            bs.quality += 2; // qualité + 2 à 10 jours ou moins
+
+        } else {
+            bs.quality += 1; // qualité + 1 à chaque jour perdu
+        }
+
+        capQuality(bs);
+    }
+    public void updateAgedBrie(Item brie) {
+        brie.sellIn--;
+        brie.quality++;
+
+        if(brie.sellIn < 0) brie.quality++;
+
+        capQuality(brie);
+    }
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            switch (item.name) {
+                case SULFURAS:
+                    updateSulfuras(item);
+                    break;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                case BACKSTAGE:
+                    updateBackStage(item);
+                    break;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+                case AGED_BRIE:
+                    updateAgedBrie(item);
+                    break;
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+                default:
+                    updateBaseQuality(item);
             }
         }
     }
+
+
+    private void capQuality(Item item) {
+        if (item.quality < MIN_CAP) item.quality = MIN_CAP;
+        if (item.quality > MAX_CAP) item.quality = MAX_CAP;
+    }
+
 }
