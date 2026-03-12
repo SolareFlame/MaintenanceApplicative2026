5e2a5267b1c83b1b5dee1d7cb0a6365f82784a5d
SolareFlame <zeldac332@gmail.com>
2026-03-12 08:57:18 +0100

    Refractor Sulfuras into own item class

---
+++ GildedRose/src/main/java/com/gildedrose/items/Sulfuras.java
@@ -0,0 +1,12 @@
+package com.gildedrose.items;
+
+public class Sulfuras extends GuildedItem {
+    public Sulfuras(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        return;
+    }
+}
