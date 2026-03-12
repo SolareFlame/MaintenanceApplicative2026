7b2b329f6da88a2d1cd1017eee6e80a0879b58b0
SolareFlame <zeldac332@gmail.com>
2026-03-12 08:56:35 +0100

    Refractor BackStage into own item class

---
+++ GildedRose/src/main/java/com/gildedrose/items/Backstage.java
@@ -0,0 +1,25 @@
+package com.gildedrose.items;
+
+public class Backstage extends GuildedItem {
+    public Backstage(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        ageSellIn();
+
+        if (getSellIn() < 0) {
+            setQuality(0); // passé
+
+        } else if (getSellIn() < 5) {
+            setQuality(getQuality() + 3); // qualité + 3 à 5 jours ou moins
+
+        } else if (getSellIn() < 10) {
+            setQuality(getQuality() + 2); // qualité + 2 à 10 jours ou moins
+
+        } else {
+            setQuality(getQuality() + 1); // qualité + 1 à chaque jour perdu
+        }
+    }
+}
